e92dad7d5c02c512e9f8a5872e1924f684fb727a
c-thun <kapoor.aakash@outlook.com>
2020-07-10 23:03:54 +0530

    safeareaview in main app

--- App.js
+++ App.js
@@ -33,35 +33,37 @@ const Stack = createStackNavigator();
 const App: () => React$Node = () => {
     return (
         <>
-            <MeridioHeader
-                backgroundColor="gainsboro"
-                largeTitleFontColor="green"
-                largeTitleFontSize={22}
-            />
-            <NavigationContainer>
-                <Tab.Navigator
-                    screenOptions={({ route }) => ({
-                        tabBarIcon: ({ focused, color, size }) => {
-                            let iconName;
+            <SafeAreaView style={{flex: 1}}>
+                <MeridioHeader
+                    backgroundColor="gainsboro"
+                    largeTitleFontColor="green"
+                    largeTitleFontSize={22}
+                />
+                <NavigationContainer>
+                    <Tab.Navigator
+                        screenOptions={({ route }) => ({
+                            tabBarIcon: ({ focused, color, size }) => {
+                                let iconName;
 
-                            if (route.name === 'Send') {
-                                iconName = (Platform.OS==='ios')?'ios-cloud-upload':'md-cloud-upload';
-                            } else if (route.name === 'Receive') {
-                                iconName = (Platform.OS==='ios')?'ios-cloud-download':'md-cloud-download';
-                            }
+                                if (route.name === 'Send') {
+                                    iconName = (Platform.OS === 'ios') ? 'ios-cloud-upload' : 'md-cloud-upload';
+                                } else if (route.name === 'Receive') {
+                                    iconName = (Platform.OS === 'ios') ? 'ios-cloud-download' : 'md-cloud-download';
+                                }
 
-                            return <Ionicons name={iconName} size={size} color={color} />;
-                        },
-                    })}
-                    tabBarOptions={{
-                        activeTintColor: 'rebeccapurple',
-                        inactiveTintColor: 'gray',
-                    }}
-                >
-                    <Tab.Screen name="Send" component={SendScreen} />
-                    <Tab.Screen name="Receive" component={ReceiveScreen} />
-                </Tab.Navigator>
-            </NavigationContainer>
+                                return <Ionicons name={iconName} size={size} color={color} />;
+                            },
+                        })}
+                        tabBarOptions={{
+                            activeTintColor: 'rebeccapurple',
+                            inactiveTintColor: 'gray',
+                        }}
+                    >
+                        <Tab.Screen name="Send" component={SendScreen} />
+                        <Tab.Screen name="Receive" component={ReceiveScreen} />
+                    </Tab.Navigator>
+                </NavigationContainer>
+            </SafeAreaView>
         </>
     );
 };
