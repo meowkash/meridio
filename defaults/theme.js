6875d5099417477ea456e746b70521726cd62270
c-thun <kapoor.aakash@outlook.com>
2020-07-14 05:06:35 +0530

    starting on Dark Mode support

---
+++ defaults/theme.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { DefaultTheme } from '@react-navigation/native';
+
+const MyTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: 'rgb(255, 45, 85)',
+  },
+};
+
