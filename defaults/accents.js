40aadc9e03784cfc737998a606c34e1f8653aa59
Liekos97 <dhruv.gupta537@gmail.com>
2020-08-02 16:44:59 +0530

    Updated dark accents

--- defaults/accents.js
+++ defaults/accents.js
@@ -2,31 +2,38 @@ export const accentColors = [
     {
         name: 'Green',
         lightHex: '#519657',
-        darkHex: '#20672d'
+        //darkHex: '#20672d'
+        darkHex: '#184e22'
     },
     {
         name: 'Blue',
         lightHex: '#64b5f6',
-        darkHex: '#01579b'
+        darkHex: '#6699cc'
     },
     {
         name: 'Pink',
         lightHex: '#f06292',
-        darkHex: '#c51162'
+        darkHex: '#ff69b4'
     },
     {
         name: 'Orange',
         lightHex: '#f57c00',
-        darkHex: '#bf360c'
+        //darkHex: '#bf360c'
+        darkHex: '#c06002'
     },
     {
         name: 'Purple',
         lightHex: '#b39ddb',
-        darkHex: '#673ab7'
+        darkHex: '#854785'
     },
     {
         name: 'Red',
         lightHex: '#f44336',
-        darkHex: '#d50000'
+        //darkHex: '#ba0021'
+        //darkHex: '#CF1020'
+        //darkHex: '#A91101'
+        //darkHex: '#e4181e'
+        //darkHex: '#850000'
+        darkHex: '#b21919'
     },
 ];
